462a42c11607b8fab826b608d53ce0a37dadaeb9
aries1711 <levfreey17@gmail.com>
2020-08-27 21:57:05 +0700

    button function

--- app/src/main/java/com/inddevid/belajar/MainActivity.kt
+++ app/src/main/java/com/inddevid/belajar/MainActivity.kt
@@ -2,8 +2,10 @@ package com.inddevid.belajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import java.io.File
 
@@ -11,8 +13,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.belajar_wallpaper)
-        val imageBg: ImageView = findViewById(R.id.bg_id)
-        Glide.with(this).load(R.drawable.belajar).into(imageBg);
+        setContentView(R.layout.activity_main)
+//        val imageBg: ImageView = findViewById(R.id.bg_id)
+//
+        val rollButton: Button = findViewById(R.id.btn_roll)
+        rollButton.setOnClickListener { rollDiced() }
+    }
+
+    private fun rollDiced() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val numberMessage: TextView = findViewById(R.id.numberRoller)
+        numberMessage.text = diceRoll.toString()
+    }
+
+    class Dice (private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
     }
 }
